aa3a8cd967ea229f70b58b9ad9f4f4b9dafc569b
okraj <hlavsa.jara@seznam.cz>
2023-12-21 17:40:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-const app = document.getElementById("app");
-const body = document.body;
-const modeToggle = document.getElementById("modeToggle");
-const modeIcon = document.getElementById("modeIcon");
-
-// Check the user's preference for dark mode
-const prefersDarkMode = window.matchMedia(
-  "(prefers-color-scheme: dark)"
-).matches;
-
-// Set initial mode based on user preference
-if (prefersDarkMode) {
-  body.classList.add("dark-mode");
-  modeIcon.innerText = "Přepnout do ☀️";
-}
-
-// Toggle dark mode
-modeToggle.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
-  const isDarkMode = body.classList.contains("dark-mode");
-  modeIcon.innerText = isDarkMode ? "Přepnout do ☀️" : "Přepnout do 🌙";
-});
-
-// JavaScript to handle scroll event
 document.addEventListener("DOMContentLoaded", (event) => {
+  const app = document.getElementById("app");
+  const body = document.body;
+  const modeToggle = document.getElementById("modeToggle");
+  const modeIcon = document.getElementById("modeIcon");
+
+  // Check the user's preference for dark mode
+  const prefersDarkMode = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+
+  // Set initial mode based on user preference
+  if (prefersDarkMode) {
+    body.classList.add("dark-mode");
+    modeIcon.innerText = "Přepnout do ☀️";
+  }
+
+  // Toggle dark mode
+  modeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+    const isDarkMode = body.classList.contains("dark-mode");
+    modeIcon.innerText = isDarkMode ? "Přepnout do ☀️" : "Přepnout do 🌙";
+  });
+
+  // JavaScript to handle scroll event
+
   window.addEventListener("scroll", function () {
     var hero = document.getElementById("hero");
     var scrollStart = 300;
