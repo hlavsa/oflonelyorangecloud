d0c5b02ae597da6f338e7b6923b7cce04cf4d501
okraj <hlavsa.jara@seznam.cz>
2023-12-21 13:16:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const app = document.getElementById('app');
+const app = document.getElementById("app");
 const body = document.body;
-const modeToggle = document.getElementById('modeToggle');
-const modeIcon = document.getElementById('modeIcon');
+const modeToggle = document.getElementById("modeToggle");
+const modeIcon = document.getElementById("modeIcon");
 
 // Check the user's preference for dark mode
-const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const prefersDarkMode = window.matchMedia(
+  "(prefers-color-scheme: dark)"
+).matches;
 
 // Set initial mode based on user preference
 if (prefersDarkMode) {
-    body.classList.add('dark-mode');
-    modeIcon.innerText = 'Přepnout do ☀️';
+  body.classList.add("dark-mode");
+  modeIcon.innerText = "Přepnout do ☀️";
 }
 
 // Toggle dark mode
-modeToggle.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-    const isDarkMode = body.classList.contains('dark-mode');
-    modeIcon.innerText = isDarkMode ? 'Přepnout do ☀️' : 'Přepnout do 🌙';
+modeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  const isDarkMode = body.classList.contains("dark-mode");
+  modeIcon.innerText = isDarkMode ? "Přepnout do ☀️" : "Přepnout do 🌙";
+});
+
+// JavaScript to handle scroll event
+window.addEventListener("scroll", function () {
+  var hero = document.getElementById("hero");
+  var fadeOutEffect = 1 - window.scrollY / (hero.clientHeight / 2);
+  hero.style.opacity = Math.max(fadeOutEffect, 0);
 });
